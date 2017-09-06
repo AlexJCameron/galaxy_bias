c476d1b1b5f83f4eb7df0d9dd05c2035a84a6645
alexc <alex.jj.cameron@gmail.com>
2017-09-06 10:13:44 +1000

    bias plot title fixed. First version run for results

--- bias.py
+++ bias.py
@@ -42,5 +42,5 @@ if __name__=='__main__':
         bias_results.write(log3 + '\n\n\n')
 
         plot_filename = results_dir + 'bias_error_%s.png' % count_type
-        this_plot_title = "Distribution of galaxy biases from Monte-Carlo bootstrapping method - counts scaled according to %s map area" % label_dict[count_type]
+        this_plot_title = "Distribution of galaxy biases from Monte-Carlo bootstrapping method\nCounts scaled according to %s map area" % label_dict[count_type]
         bootstrap.plot_bias_hist(bias_set, plot_filename, show=False, plot_title=this_plot_title)
